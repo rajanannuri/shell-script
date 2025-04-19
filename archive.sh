c90228c3a4cf549a89bb4e22420035f844bef7a4
rajanannuri <153515266+rajanannuri@users.noreply.github.com>
2025-04-19 14:42:33 +0530

    Create archive.sh

---
+++ archive.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#creation archive for 30 days
+sourcedir=/var/log
+archivedir=/archive
+days=30
+# we need to create a archival folder
+if [ -d $archivedir ];then
+echo " folder : $archivedir available"
+else
+echo "create $archivedir folder"
+mkdir -p $archivedir
+echo "$archivedir created successfully"
+fi
+# we need to find files now
+find $sourcedir -type f -mtime +$days -exec mv {} $archivedir \;
+# verifying files successfully moved or not
+if [ $? -eq 0 ];then
+echo "files older then $days moved successfully"
+else
+echo " files failed to archive"
+fi
