ded9ad11945a7447a8d8c1eba7eed8644ee8722d
rajanannuri <153515266+rajanannuri@users.noreply.github.com>
2025-04-19 15:38:50 +0530

    Update DiskUsage.sh

--- DiskUsage.sh
+++ DiskUsage.sh
@@ -7,9 +7,13 @@ df -h | grep '^/dev/' | while read line;
 do
 usage=$(echo $line | awk '{print $5}' | sed 's/%//g')
 mount=$(echo $line | awk '{print $6}')
-# Using -ge for numeric comparision
+# Using -ge for numeric comparison
 if [ $usage -ge $THRESHOLD ];
 then
 echo " Disk usage:${usage}% on $mount"
 fi
 done
+#####################################
+#df -h | grep '/dev/' | awk '{print $5}' | sed 's/%//g'
+# sed -i 's/taj/bharathi/g' usercreation.sh
+# old file to new file or oldname to new name
